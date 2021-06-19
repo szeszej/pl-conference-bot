0c7f48277383d75f75f0f8ee92113cc67592b377
unknown <>
2021-06-19 21:28:45 +0200

    added questions for 2hg

--- index.js
+++ index.js
@@ -1,118 +1,103 @@
-require('dotenv').config()
-const questions = require('./questions-2hg')
-const Discord = require('discord.js')
-const bot = new Discord.Client()
+require("dotenv").config();
+const questions = require("./questions-2hg");
+const Discord = require("discord.js");
+const bot = new Discord.Client();
 
-const TOKEN = process.env.TOKEN
+const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN)
+bot.login(TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`)
-})
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
 
 const multipleChoice = (answers, msg) => {
   answers.forEach((answer, index) =>
     msg.channel
-      .send((index + 10).toString(36).toUpperCase() + ') ' + answer)
-      .then((msg) => msg.react('✅')),
-  )
-}
+      .send((index + 10).toString(36).toUpperCase() + ") " + answer)
+      .then((msg) => msg.react("✅"))
+  );
+};
 
 const multipleYesno = (answers, msg) => {
   answers.forEach((answer, index) =>
     msg.channel
-      .send((index + 10).toString(36).toUpperCase() + ') ' + answer)
+      .send((index + 10).toString(36).toUpperCase() + ") " + answer)
       .then((res) => {
-        res.react('👍')
-        res.react('👎')
-      }),
-  )
-}
+        res.react("👍");
+        res.react("👎");
+      })
+  );
+};
 
 const sequence = (answers, msg) => {
   answers.forEach(
     (answer, index) =>
       msg.channel
-        .send((index + 10).toString(36).toUpperCase() + ') ' + answer)
+        .send((index + 10).toString(36).toUpperCase() + ") " + answer)
         .then((msg) => {
-          msg.react('1️⃣')
-          msg.react('2️⃣')
-          msg.react('3️⃣')
-          msg.react('4️⃣')
-          msg.react('5️⃣')
-          msg.react('6️⃣')
-        }),
+          msg.react("1️⃣");
+          msg.react("2️⃣");
+          msg.react("3️⃣");
+          msg.react("4️⃣");
+          msg.react("5️⃣");
+          msg.react("6️⃣");
+        })
     //   .then((msg) => msg.message.react('2️⃣'))
     //   .then((msg) => msg.message.react('3️⃣'))
     //   .then((msg) => msg.message.react('4️⃣'))
     //   .then((msg) => msg.message.react('5️⃣'))
     //   .then((msg) => msg.message.react('6️⃣'))
-  )
-}
+  );
+};
 
 //Implement a Command Handler? https://www.sitepoint.com/discord-bot-node-js/
 //https://discordjs.guide/
 
-bot.on('message', (msg) => {
+bot.on("message", (msg) => {
   if (
-    msg.content.includes('!pytanie') &&
-    msg.author.id === '234760750583775242'
+    msg.content.includes("!pytanie") &&
+    msg.author.id === "234760750583775242"
   ) {
-    const questionNumber = parseInt(msg.content.replace('!pytanie ', ''))
-    const question = questions[questionNumber - 1]
+    const questionNumber = parseInt(msg.content.replace("!pytanie ", ""));
+    const question = questions[questionNumber - 1];
     if (!question) {
-      return msg.channel.send('Nie ma takiego pytania!')
+      return msg.channel.send("Nie ma takiego pytania!");
     }
     msg.channel
-      .send(questionNumber + '. ' + question.question)
+      .send(questionNumber + ". " + question.question)
       .then((msg) => {
-        if (question.type === 'multiple') {
-          multipleChoice(question.answers, msg)
-        } else if (question.type === 'yesno') {
-          msg.react('👍').then((res) => msg.react('👎'))
-        } else if (question.type === 'sequence') {
-          sequence(question.answers, msg)
-        } else if (question.type === 'multipleYesno') {
-          multipleYesno(question.answers, msg)
-        } else if (question.type === 'rating') {
-          msg.react('1️⃣')
-          msg.react('2️⃣')
-          msg.react('3️⃣')
-          msg.react('4️⃣')
-          msg.react('5️⃣')
+        if (question.type === "multiple") {
+          multipleChoice(question.answers, msg);
+        } else if (question.type === "yesno") {
+          msg.react("👍").then((res) => msg.react("👎"));
+        } else if (question.type === "sequence") {
+          sequence(question.answers, msg);
+        } else if (question.type === "multipleYesno") {
+          multipleYesno(question.answers, msg);
+        } else if (question.type === "rating") {
+          msg.react("1️⃣");
+          msg.react("2️⃣");
+          msg.react("3️⃣");
+          msg.react("4️⃣");
+          msg.react("5️⃣");
         }
       })
-      .catch((error) => console.log('coś się jebło szeszej'))
+      .catch((error) => console.log("coś się jebło szeszej"));
   } else if (
-    msg.content.includes('!link' && msg.author.id === '234760750583775242')
+    msg.content.includes("!intro") &&
+    msg.author.id === "234760750583775242"
   ) {
     msg.channel
       .send(
-        // 'Link do egzaminu z DMTR: https://judgeacademy.com/courses/mtg-digital-magic-tournament-rules/',
-        'Link do egzaminu z DIPG: https://judgeacademy.com/courses/mtg-digital-infraction-procedure-guide/',
+        `First a small reminder on the basic differences between 2HG and 1v1:
+
+        There are :two: teams :two: players each
+        Each team has a shared lifetotal that starts at :three: :zero:
+        Teams have shared turns (each player on the team has upkeep at the same time, etc.).`
       )
-      .catch((error) => console.log('coś się jebło szeszej'))
-  } else if (msg.content === '!spis 1') {
-    msg.channel
-      .send(
-        'Zapraszamy do wypełnienia formularza: https://forms.gle/Y7VnphSbyf38cru77 Jest on wymagany, aby potwierdzić Waszą obecność na niniejszej konferencji!',
-      )
-      .then((msg) => {})
-      .catch((error) => console.log('coś się jebło szeszej'))
-  } else if (msg.content === '!spis 2') {
-    msg.channel
-      .send(
-        'Zapraszamy do wypełnienia formularza: https://forms.gle/MJQURsCzNLxd976GA Jest on wymagany, aby potwierdzić Waszą obecność na niniejszej konferencji!',
-      )
-      .then((msg) => {})
-      .catch((error) => console.log('coś się jebło szeszej'))
-  } else if (msg.content === '!feedback') {
-    msg.channel
-      .send(
-        'Zapraszamy do wypełnienia formularza z feedbackiem dotyczącym prezentacji: https://forms.gle/VouruMCFv2PfdHqr5',
-      )
-      .then((msg) => {})
-      .catch((error) => console.log('coś się jebło szeszej'))
+      .catch((error) => console.log("coś się jebło szeszej"));
   }
-})
+});
+
+//https://blogs.magicjudges.org/2hg/faq/
--- questions-2hg.js
+++ questions-2hg.js
@@ -1,166 +1,195 @@
 const questions = [
   {
-    question: 'DMTR zawiera zasady prowadzenia następujących turniejów:',
-    type: 'multiple',
+    question: "How does mulliganing work in 2HG?",
+    type: "multiple",
     answers: [
-      'Turniejów przeprowadzanych Online',
-      'Turniejów w sieci LAN (lokalnych)',
-      'Oba powyższe',
-      'Żadne z powyższych',
+      "The players on the starting team, one by one, take mulligans until they keep their hands, then the same follows for the other team",
+      "The players on the starting team take mulligans simultaneously until they keep their hands, then the same follows for the other team",
+      "The players on the starting team take a mulligan simultaneously, then the same happens for the other team. Repeat until every player has kept their hand.",
+    ],
+  },
+  {
+    question: "Do players get a free mulligan?",
+    type: "yesno",
+  },
+  {
+    question:
+      "Do players in the starting team skip their first draw step in the game?",
+    type: "yesno",
+  },
+  {
+    question:
+      "Altair asks their teammate, Allgemein whether they should keep their hand or mulligan. Nanite immediately calls a judge. What is the appropriate punishment, if any?",
+    type: "multiple",
+    answers: [
+      "Altair receives a Match Loss for Outside Assistance",
+      "Altair receives a Warning for Mulligan Procedure Violation",
+      "Altair receives no punishment, consulting with your teammate is legal in 2HG",
     ],
   },
   {
     question:
-      'W przypadku kiedy informacje turniejowe (factsheet) i DMTR zawierają sprzeczne instrukcje:',
-    type: 'multiple',
+      "Athena, sitting on the right, and Albany, sitting on the left, can’t agree as to whether Albany should take a mulligan. Athena says that Albany should do it, Albany disagrees. What is the proper resolution?",
+    type: "multiple",
     answers: [
-      'Ważniejszy jest factsheet',
-      'Ważniejszy jest DMTR',
-      'Organizator decyduje, która instrukcja ma zastosowanie',
-      'Sędzia główny decyduje, która instrukcja ma zastosowanie',
-    ],
-  },
-  {
-    question: 'Rola Lead Admin w DMTR oznacza:',
-    type: 'multiple',
-    answers: [
-      'Organizatora',
-      'Sędziego głównego',
-      'Teamleadera',
-      'Scorekeepera',
-    ],
-  },
-  {
-    question: 'Czy DMTR wymaga od gracza stabilnego łącza?',
-    type: 'yesno',
-  },
-  {
-    question: 'Czy DMTR wymaga od gracza zaktualizowanego klienta?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Czy DMTR wymaga od gracza dołączenia do i uważania na Discord/MTGMelee/inny kanał komunikacyjny?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Procedury przed grą oczywiście też różnią się pomiędzy medium cyfrowym i papierowym. Ułóżcie więc w kolejności od 1 do 6 (reagując odpowiednimi emotkami) następujące kroki, jakie gracze muszą podjąć przed grą:',
-    type: 'sequence',
-    answers: [
-      'Wprowadź ID przeciwnika do Arena',
-      'Wybierz talię, którą zarejestrowałeś w evencie',
-      'Uruchom Arenę i wybierz Direct Challenge',
-      'Wyślij Challenge',
-      'Wybierz odpowiedni rodzaj Challenge',
-      'Znajdź Arena ID przeciwnika',
+      "Each players is responsible for their own choices, Albany cannot be forced to take a mulligan",
+      "Albany has to take the mulligan",
+      "Players flip a coin to settle the dispute",
+      "Players need to call a judge to settle the dispute",
     ],
   },
   {
     question:
-      'Czy według DMTR dopuszczane są intentional draws (celowe remisy)?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Co dzieje się, gdy gracze grają dalej po zakończeniu czasu rundy?',
-    type: 'multiple',
+      "Alberta begins their main phase with !card Sorin, Lord of Innistrad! and !card Grizzly Bear! on the battlefield. Their teammate, Alex, controls !card Forest Bear!. When Alberta activates the second ability of Sorin, what will be the stats of each of the bears?",
+    type: "multiple",
     answers: [
-      'Kontaktujemy się z nimi, żeby poinformować ich, że mogą zrobić jeszcze maksymalnie 5 tur w grze',
-      'Dajemy im podwójną przegraną',
-      'Wynik gry zostaje taki, jaki był w momencie skończenia się czasu',
-      'Kontaktujemy się z nimi, aby dowiedzieć się, jak im idzie',
+      "Both will be 2/2s",
+      "Grizzly Bear will be a 2/2 and Forest Bear will be a 3/2",
+      "Grizzly Bear will be a 3/2 and Forest Bear will be a 2/2",
+      "Both will be 3/2s",
     ],
   },
   {
     question:
-      'Czy według DMTR gracze mają dostęp do swojej decklisty w każdym momencie?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Jeśli gracz dropuje, a turniej jest rozgrywany w trybie single/double elimination, co robimy z resztą drabinki?',
-    type: 'multiple',
+      "Adelaida and Abigail each have !card Ajani’s Pridemate! in play. Adelaida casts !card Vampiric Touch! targeting one of the opponents. After Vampiric Touch resolves and the stack is empty, how many counters will be on each Pridemate?",
+    type: "multiple",
     answers: [
-      'Zmieniamy całą drabinkę, żeby odzwierciedlić dropa',
-      'Traktujemy tę sytuację tak jakby gracz otrzymał Match Lossa w każdej kolejnej rundzie',
-      'Każdy kolejny jego mecz zakończy się remisem',
-      'Jego przeciwnicy dostaną bye w każdym z meczu, w którym mieliby z nim grać',
-    ],
-  },
-  {
-    question: 'Czy według DMTR gracze mogą robić notatki podczas gier?',
-    type: 'yesno',
-  },
-  {
-    question: 'Czy według DMTR gracze mogą streamować swoje rozgrywki?',
-    type: 'multiple',
-    answers: [
-      'Tak, bez ograniczeń',
-      'Tak, ale tylko jeśli jest to feature match',
-      'Tak, ale zaleca się ustawienie minimum 2-minutowego opóźnienia',
-      'Nie',
+      "Both will have 1 counter",
+      "Adeilada’s will have 1 counter, Abigail’s will have 0 counters",
+      "Adeilada’s will have 0 counters, Abigail’s will have 1 counter",
+      "Both will have 0 counters",
     ],
   },
   {
     question:
-      'Czy według DMTR sędzia główny może używać powtórek video podczas wydawania rulingów/prowadzenia dochodzenia?',
-    type: 'yesno',
-  },
-  {
-    question: 'Jakie są rodzaje informacji w DMTR?',
-    type: 'multiple',
+      "Armagerd and Armin are at 30 life and are playing against Noscope and Nitro, also at 30 life. Armin controls a 1/1 Vampire token and casts !card Arterial Flow!. Assuming that Noscope and Nitro previously had 2 cards in hand each, what happens after the Flow resolves?",
+    type: "multiple",
     answers: [
-      'Private i public',
-      'Status, free, private i public',
-      'Open i closed',
-      'Public, derived i private',
+      "The active team will be at 32 life, the non-active team will be at 28 and neither of its members will have any cards in their hands",
+      "The active team will be at 32 life, the non-active team will be at 26 and one of its members (the non-active team’s choice) will have no cards in their hand",
+      "The active team will be at 34 life, the non-active team will be at 26 and neither of its members will have any cards in their hands ",
+      "The active team will be at 32 life, the non-active team will be at 26 and neither of its members will have any cards in their hands ",
     ],
   },
   {
     question:
-      'Czy według DMTR gracze mogą prosić przeciwników o podanie im private information?',
-    type: 'yesno',
-  },
-  {
-    question: 'Jakie zasady komunikacji zawiera DMTR?',
-    type: 'multiple',
+      "Anna on the left and Arven on the right are sitting opposite, respectively, Nolan and Nadina. Arven wants to attack with their !card Abyssal Specter!. Assuming there are no blocks or removal spells, how can they make sure that Nolan discards a card with the trigger of the Specter?",
+    type: "multiple",
     answers: [
-      'Gracze mają być mili i szybko odpowiadać osobom funkcyjnym',
-      'Takie same jak w MTR',
-      'Graczom nie wolno się ze sobą komunikować z wyjątkiem procedur przez meczem',
-      'Graczom wolno komunikować się jedynie z osobami funkcyjnymi',
+      "Arven needs to declare the Specter as attacking Nolan",
+      "During combat damage step, Arven needs to assign the Specter’s damage to Nolan",
+      "It’s not possible, players always attack the opponent directly in front of them",
+      "Arven doesn’t need to do or say anything, creatures attack the opponent on the left by default",
     ],
   },
   {
     question:
-      'Czy według DMTR gracze mogą zastraszać lub nękać przeciwników używając social media albo ogólnodostępnych czatów?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Czy DMTR (w związku z obecnością zegara szachowego na Arenie) uwzględnia w ogóle takie przewinienia jak Slow Play czy Stalling podczas eventów cyfrowych?',
-    type: 'yesno',
-  },
-  {
-    question:
-      'Gracz przez Discord dostaje od swojego znajomego informacje dotyczące tego, co jego przeciwnik ma na ręce. Gracz nie prosił swojego znajomego o te informacje. Co powinien zrobić?',
-    type: 'multiple',
+      "Anna on the left and Arven on the right are sitting opposite, respectively, Nolan and Nadina. Arven attacks with their !card Abyssal Specter! saying nothing. There are no blocks. What happens in the combat damage step?",
+    type: "multiple",
     answers: [
-      'Poinformować o tym fakcie przeciwnika i powstrzymać się od podejmowania decyzji bazując na tych informacjach',
-      'Usunąć wiadomość, zablokować znajomego i powstrzymać się od podejmowania decyzji bazując na tych informacjach',
-      'Zignorować otrzymane informacje',
-      'Poinformować o tym fakcie osoby funkcyjne i powstrzymać się od podejmowania decyzji bazując na tych informacjach',
+      "Both Nolan and Nadina discard a card",
+      "Nolan discards a card",
+      "Nadina discards a card",
+      "Neither players discards a card",
     ],
   },
   {
-    question: 'Czy nauczyliście się dzisiaj czegoś nowego?',
-    type: 'yesno',
+    question:
+      "Alokar, Anastasia’s teammate, has a !card Grizzly Bear! on the battlefield. Nathalia, Nelly’s teammate, controls !card Ghostly Prison!. The active team declares Grizzly Bear as the attacker with Alokar saying “I attack Nelly”. What happens?",
+    type: "multiple",
+    answers: [
+      "Alokar may attack with the Bear as normal",
+      "Alokar still needs to pay 2 mana to attack",
+      "Alokar’s team still needs to pay 2 mana to attack",
+      "This can’t happen as described, teams attack the other team, not specific players",
+    ],
   },
   {
-    question: 'Jak podobał się Wam seminar?',
-    type: 'rating',
+    question:
+      "Aran declares !card Bane of Bala Ged! as attacking Noddy, Notty’s teammate. What happens when Bane’s triggered ability resolves?",
+    type: "multiple",
+    answers: [
+      "Both Noddy and Notty have to exile two permanents",
+      "Noddy has to exile two permanents",
+      "Aran gets to choose which of the defending players has to exile two permanents",
+    ],
   },
-]
+  {
+    question:
+      "Once the Bane attacks Noddy, Notty wants to help his teammate by blocking it with !card Grizzly Bear!. Are they allowed to block creatures attacking their teammate?",
+    type: "yesno",
+  },
+  {
+    question:
+      "Alan begins their upkeep controlling !card Verdant Force!. How many Saprolings will be created with its triggered ability this upkeep?",
+    type: "multiple",
+    answers: [1, 2, 3, 4],
+  },
+  {
+    question:
+      "Alan begins their upkeep controlling !card Barbed Wire!. Assuming nothing else happens in this upkeep, how much damage will be dealt to Alan’s team this step?",
+    type: "multiple",
+    answers: [1, 2, 3, 4],
+  },
+  {
+    question:
+      "Alice, Alonse’s teammate, casts !card Time Warp! targeting themselves, then the team finishes their turn. What happens?",
+    type: "multiple",
+    answers: [
+      "The other team gets to do their turn since extra turns don’t work in 2HG",
+      "Alice will get another turn but Alonse won’t",
+      "Alice’s team will get an extra turn",
+      "This can’t happen as described, extra turn spells cannot be used in 2HG",
+    ],
+  },
+  {
+    question:
+      "Arsen controls !card Sorin Markov! and targets Nobby with the second ability. Nobby’s team is at 20 life. What happens?",
+    type: "multiple",
+    answers: [
+      "Nobby’s team’s lifetotal becomes 10",
+      "To make Nobby and Nobby’s teammate lifetotal become 10, both lose 10 life. Then the team’s lifetotal becomes 0",
+      "Nobby’s team’s lifetotal stays at 20 since in 2HG players don’t have life as individuals",
+    ],
+  },
+  {
+    question:
+      "Arnold activates !card Door to Nothingness! targeting Nelly. What happens when the ability resolves?",
+    type: "multiple",
+    answers: [
+      "Nelly loses the game, their teammate can continue playing as normal",
+      "Both players lose the game as a team",
+      "Nothingness happens, these types of effect don’t function in 2HG",
+    ],
+  },
+  {
+    question:
+      "Arney and Al attack Nina and Nobby with !card Glistener Elf!. Nina and Nobby already have 9 poison counters on them. There are no blocks. What happens in the combat damage step?",
+    type: "multiple",
+    answers: [
+      "Nina and Nobby lose the game due to having 10 poison counters",
+      "Nina and Nobby now have a total of 10 poison counters but may continue playing",
+      "This can’t happen as described, players get poison counters individually, not as a team",
+    ],
+  },
+  {
+    question:
+      "In a 2HG draft, how many cards do each team pick before passing the booster pack?",
+    type: "multiple",
+    answers: [0.75, 1, 2, "2HG can’t be run as draft"],
+  },
+  {
+    question: "Are 2HG matches played until 2 wins?",
+    type: "yesno",
+  },
+  {
+    question: "Have you learned anything new during this seminar?",
+    type: "yesno",
+  },
+  {
+    question: "How did you like the seminar?",
+    type: "rating",
+  },
+];
 
-module.exports = questions
+module.exports = questions;
